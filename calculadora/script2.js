a071c62c2e5a5d40f984612c5f41f54c4e2115e8
Aquino Matìas <aquinito2092@gmail.com>
2022-10-21 08:28:33 -0300

    cal

---
+++ calculadora/script2.js
@@ -0,0 +1,11 @@
+
+function pres(tecla){
+    resultado = document.getElementById("resultado").value
+    document.getElementById("resultado").value = resultado + tecla
+    console.log()
+}
+
+borrar() {
+    this.NumActual = this.Num-Actual.toString().slice(0,-1);
+    this.ImprimirValores();
+}
