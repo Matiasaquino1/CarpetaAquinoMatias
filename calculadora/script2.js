3b7e800d994391ff7e828cae8750f4500a7bf311
Aquino Matìas <aquinito2092@gmail.com>
2022-10-27 11:32:32 -0300

    min

--- calculadora/script2.js
+++ calculadora/script2.js
@@ -1,11 +1,29 @@
+var display = 0;
+var numero1 = 0;
+var numero2 = 0;
+var curso = 0;
+var total = 0;
+
+
 
 function pres(tecla){
-    resultado = document.getElementById("resultado").value
-    document.getElementById("resultado").value = resultado + tecla
-    console.log()
+    ///display = document.getElementById("display").value;
+    //document.getElementById("display").value = display + tecla;
+    //display = parseInt(document.getElementById("display").value);
+    if (total == 0){
+        document.getElementById("display").value += tecla;
+        numero1 = document.getElementById("display").value;
+    }else{
+        document.getElementById("display").value += tecla;
+        numero2 = document.getElementById("display").value;
+    }
+
+    console.log();
 }
 
-borrar() {
-    this.NumActual = this.Num-Actual.toString().slice(0,-1);
-    this.ImprimirValores();
+function restar(){
+    document.getElementById("display").value = "";
+    total = parseInt(numero1) - parseInt(numero2);
+    console.log();
 }
+
